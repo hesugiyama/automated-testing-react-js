5e52c1aa5113a03c5490610fdf1a48a389a1429e
Sugiyama <Henrique.Sugiyama@dieboldnixdorf.com>
2020-02-28 14:16:13 -0300

    Changed nomenclature 'test' to 'describe' and 'it'

--- src/Components/RepoList/index.test.js
+++ src/Components/RepoList/index.test.js
@@ -3,42 +3,43 @@ import {render, fireEvent, waitForElementToBeRemoved} from '@testing-library/rea
 
 import RepoListHooks from './index.js';
 
-test('Should be able to render first item', () => {
-  const {container} = render(
-    <RepoListHooks />
-  );
+describe('RepoList component', () => {
+  it('Should be able to render first item', () => {
+    const {container} = render(
+      <RepoListHooks />
+    );
 
-  const el = container.querySelector('#NodeJS');
-  expect(!!el).toBeTruthy();
-});
+    const el = container.querySelector('#NodeJS');
+    expect(!!el).toBeTruthy();
+  });
 
+  it('Should be able to add new repositories', () => {
+    const {container} = render(
+      <RepoListHooks />
+    );
 
-test('Should be able to add new repositories', () => {
-  const {container} = render(
-    <RepoListHooks />
-  );
+    const input = container.querySelector('#new-repo-input');
+    const form = container.querySelector('#new-repo-form');
+    fireEvent.change(input, {target: {value: 'ReactJS'}});
+    fireEvent.submit(form);
+    expect(!!container.querySelector('#ReactJS')).toBeTruthy();
+    expect(input.value).toBe('');
+    console.log("input value: ", input.value);
+  });
 
-  const input = container.querySelector('#new-repo-input');
-  const form = container.querySelector('#new-repo-form');
-  fireEvent.change(input, {target: {value: 'ReactJS'}});
-  fireEvent.submit(form);
-  expect(!!container.querySelector('#ReactJS')).toBeTruthy();
-  expect(input.value).toBe('');
-  console.log("input value: ", input.value);
-});
+  it('Should be able to remove a repository', () => {
+    const {container} = render(
+      <RepoListHooks />
+    );
 
-test('Should be able to remove a repository', () => {
-  const {container} = render(
-    <RepoListHooks />
-  );
+    const input = container.querySelector('#new-repo-input');
+    const form = container.querySelector('#new-repo-form');
 
-  const input = container.querySelector('#new-repo-input');
-  const form = container.querySelector('#new-repo-form');
+    fireEvent.change(input, {target: {value: 'ReactJS'}});
+    fireEvent.submit(form);
 
-  fireEvent.change(input, {target: {value: 'ReactJS'}});
-  fireEvent.submit(form);
-
-  const button = container.querySelector('#ReactJS-remove-button');
-  waitForElementToBeRemoved(() => container.querySelector('#ReactJS'));
-  fireEvent.click(button);
+    const button = container.querySelector('#ReactJS-remove-button');
+    waitForElementToBeRemoved(() => container.querySelector('#ReactJS'));
+    fireEvent.click(button);
+  });
 });
